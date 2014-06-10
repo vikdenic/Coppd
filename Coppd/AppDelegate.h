09c6eeba7427c662f255f0080b3e96ee4db88bf0
vikdenic <vik.denic@gmail.com>
2014-06-09 19:20:49 -0500

    Initial Commit

---
+++ Coppd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Coppd
+//
+//  Created by Vik Denic on 6/9/14.
+//  Copyright (c) 2014 Aaron & Vik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
